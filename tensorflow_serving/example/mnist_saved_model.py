64cdcef47f3f984105940f5612ff8e66ef35148f
Ellison Chan <ellison.chan@gmail.com>
2018-05-28 23:58:40 -0700

    Version 18

--- tensorflow_serving/example/mnist_saved_model.py
+++ tensorflow_serving/example/mnist_saved_model.py
@@ -38,7 +38,7 @@ import mnist_input_data
 
 tf.app.flags.DEFINE_integer('training_iteration', 1000,
                             'number of training iterations.')
-tf.app.flags.DEFINE_integer('model_version', 17, 'version number of the model.')
+tf.app.flags.DEFINE_integer('model_version', 18, 'version number of the model.')
 tf.app.flags.DEFINE_string('work_dir', '/tmp', 'Working directory.')
 FLAGS = tf.app.flags.FLAGS
 
